b8c04a0d4301b483c0080de8e299699c1570ad02
BLK LUV [org] <info@blkluv.org>
2023-10-29 03:20:51 -0400

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import LiveWidget from "./LiveWidget";
 export default function Home() {
 	return (
 		<main className="flex min-h-screen flex-col items-center p-24">
-			<h2 className="text-3xl font-extrabold">HealXYZ TikTok Live</h2>
+			<h2 className="text-3xl font-extrabold">HealXYZ 🔴 Live</h2>
 			<LiveWidget />
            		<a className="hover:underline text-blue-900" href="https://discord.com/channels/1165931081564946443/role-subscriptions">Join the discord</a>
 		</main>
