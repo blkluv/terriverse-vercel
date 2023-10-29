290867755cc59c2c32166488df7a8b4e051e970a
BLK LUV [org] <info@blkluv.org>
2023-10-29 03:06:59 -0400

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,9 +3,9 @@ import LiveWidget from "./LiveWidget";
 export default function Home() {
 	return (
 		<main className="flex min-h-screen flex-col items-center p-24">
-			<h2 className="text-3xl font-extrabold">The Terriverse</h2>
+			<h2 className="text-3xl font-extrabold">HealXYZ TikTok Live</h2>
 			<LiveWidget />
-           		<a className="hover:underline text-blue-900" href="https://discord.gg/4fYPy4Kx6T">Join the discord</a>
+           		<a className="hover:underline text-blue-900" href="https://discord.com/channels/1165931081564946443/role-subscriptions">Join the discord</a>
 		</main>
 	);
 }
